63f404f56de35940b1ea05c91a7248b346141e9d
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 01:52:25 +0200

    Initial Commit

---
+++ CounterByAppleDocs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CounterByAppleDocs
+//
+//  Created by Marcin Jędrzejak on 02/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CounterByAppleDocs/CounterByAppleDocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterByAppleDocsApp.swift
+//  CounterByAppleDocs
+//
+//  Created by Marcin Jędrzejak on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CounterByAppleDocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
