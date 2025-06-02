f4635b275b709d111341415b1783d6e2852a2478
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 16:13:35 +0200

    feat: Managing a timer (completed Adding side effects)

--- CounterByAppleDocs/CounterFeature.swift
+++ CounterByAppleDocs/CounterFeature.swift
@@ -15,6 +15,7 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
 
     enum Action {
@@ -22,7 +23,11 @@ struct CounterFeature {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+
+    enum CancelID { case timer }
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -67,6 +72,25 @@ struct CounterFeature {
                 state.count += 1
                 state.fact = nil
                 return .none
+
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
@@ -99,6 +123,15 @@ struct CounterView: View {
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             }
+
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+
             Button("Fact") {
                 store.send(.factButtonTapped)
             }
