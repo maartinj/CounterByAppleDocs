f5528ad614cc227149c580ceb67024885a221c86
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 02:19:02 +0200

    feat: Integrating into the app (completed Your first feature)

--- CounterByAppleDocs/CounterByAppleDocsApp.swift
+++ CounterByAppleDocs/CounterByAppleDocsApp.swift
@@ -5,13 +5,19 @@
 //  Created by Marcin Jędrzejak on 02/06/2025.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct CounterByAppleDocsApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: CounterByAppleDocsApp.store)
         }
     }
 }
