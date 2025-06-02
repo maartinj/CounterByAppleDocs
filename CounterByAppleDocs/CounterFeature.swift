980c23efc809bed8b681f3211532aeb60deafb55
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 12:43:04 +0200

    feat: What is a side effect

--- CounterByAppleDocs/CounterFeature.swift
+++ CounterByAppleDocs/CounterFeature.swift
@@ -13,10 +13,13 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
 
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
 
@@ -25,10 +28,26 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
+                return .none
+
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(state.count)")!)
+                // 🛑 'async' call in a function that does not support concurrency
+                // 🛑 Errors thrown from here are not handled
+
+                state.fact = String(decoding: data, as: UTF8.self)
+                state.isLoading = false
+
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
             }
         }
@@ -61,6 +80,22 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                if store.isLoading {
+                    ProgressView()
+                } else if let fact = store.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
             }
         }
     }
