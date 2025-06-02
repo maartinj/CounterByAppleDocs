7d82eb8437c6b8716d15cc3ee257ac91966c0f3c
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-03 00:30:59 +0200

    feat: Project set up

---
+++ CounterByAppleDocs/ContactsFeature.swift
@@ -0,0 +1,75 @@
+//
+//  ContactsFeature.swift
+//  CounterByAppleDocs
+//
+//  Created by Marcin Jędrzejak on 03/06/2025.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+@Reducer
+struct ContactsFeature {
+    @ObservableState
+    struct State: Equatable {
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+    enum Action {
+        case addButtonTapped
+    }
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                // TODO: Handle action
+                return .none
+            }
+        }
+    }
+}
+
+struct ContactsView: View {
+    let store: StoreOf<ContactsFeature>
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(store.contacts) { contact in
+                    Text(contact.name)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
+}
