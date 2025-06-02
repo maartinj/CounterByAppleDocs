ec6f5aada445d53d488e3a80796ad00c9b4e7db0
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 22:13:33 +0200

    feat: Testing network requests

--- CounterFeatureTests/CounterFeatureTests.swift
+++ CounterFeatureTests/CounterFeatureTests.swift
@@ -81,4 +81,31 @@ struct CounterFeatureTests {
             $0.isTimerRunning = false
         }
     }
+
+    @Test
+    func numberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        // ❌ An effect returned for this action is still running.
+        //    It must complete before the end of the test. …
+
+        await store.receive(\.factResponse, timeout: .seconds(1)) {
+            $0.isLoading = false
+            $0.fact = "???"
+        }
+        // ❌ A state change does not match expectation: …
+        //
+        //       CounterFeature.State(
+        //         count: 0,
+        //     −   fact: "???",
+        //     +   fact: "0 is the atomic number of the theoretical element tetraneutron.",
+        //         isLoading: false,
+        //         isTimerRunning: false
+        //       )
+    }
 }
