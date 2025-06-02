7fbe18ef1ec3de1c4764fe43d854d42056bcfa94
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-02 02:13:47 +0200

    feat: Integrating with SwiftUI

--- CounterByAppleDocs/CounterFeature.swift
+++ CounterByAppleDocs/CounterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import SwiftUI
 
 @Reducer
 struct CounterFeature {
@@ -32,4 +33,43 @@ struct CounterFeature {
             }
         }
     }
+}
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+    CounterView(
+        store: Store(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+    )
 }
