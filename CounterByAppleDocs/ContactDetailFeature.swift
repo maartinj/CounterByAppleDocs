498d6f1b7581a6ae2e6187bf3ac792b337b4f5ee
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-06-05 00:29:56 +0200

    feat: Contact detail feature

---
+++ CounterByAppleDocs/ContactDetailFeature.swift
@@ -0,0 +1,51 @@
+//
+//  ContactDetailFeature.swift
+//  CounterByAppleDocs
+//
+//  Created by Marcin Jędrzejak on 05/06/2025.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct ContactDetailFeature {
+    @ObservableState
+    struct State: Equatable {
+        let contact: Contact
+    }
+
+    enum Action {
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            }
+        }
+    }
+}
+
+struct ContactDetailView: View {
+    let store: StoreOf<ContactDetailFeature>
+
+    var body: some View {
+        Form {
+        }
+        .navigationTitle(Text(store.contact.name))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ContactDetailView(
+            store: Store(
+                initialState: ContactDetailFeature.State(
+                    contact: Contact(id: UUID(), name: "Blob")
+                )
+            ) {
+                ContactDetailFeature()
+            }
+        )
+    }
+}
